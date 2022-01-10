624b6c5a29313a6511b726529f08e7f2be2a2f57
juandelia03 <juanchif5017@gmail.com>
2022-01-10 06:08:46 -0300

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,4 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules(?!(\/|\\)pdfjs-dist)/,
-      loader: "babel-loader",
-      options: {
-        presets: ["@babel/preset-env"],
-        plugins: ["@babel/plugin-proposal-optional-chaining"],
-      },
-    },
-  ],
+
 };
